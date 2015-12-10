f22e6069ea9594c9401e581e6fbc9558982811b8
Andreas Schjønhaug <andreas@schjonhaug.com>
2015-12-10 11:58:09 +0700

    Added support for Carthage.

---
+++ PIDatePicker/PIDatePicker.h
@@ -0,0 +1,19 @@
+//
+//  PIDatePicker.h
+//  PIDatePicker
+//
+//  Created by Andreas Schjønhaug on 10.12.2015.
+//  Copyright © 2015 Prolific Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PIDatePicker.
+FOUNDATION_EXPORT double PIDatePickerVersionNumber;
+
+//! Project version string for PIDatePicker.
+FOUNDATION_EXPORT const unsigned char PIDatePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PIDatePicker/PublicHeader.h>
+
+
