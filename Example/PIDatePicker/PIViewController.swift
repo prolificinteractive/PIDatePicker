ac2e8b095b96056a2fa50854fd36c123424bf886
Daniel Vancura <daniel_vancura@me.com>
2015-12-27 14:40:25 +0100

    Replaced PIDatePicker with an extension for UIDatePicker and changed the example accordingly.

--- Example/PIDatePicker/PIViewController.swift
+++ Example/PIDatePicker/PIViewController.swift
@@ -11,16 +11,20 @@ import PIDatePicker
 
 class PIViewController : UIViewController {
     
-    @IBOutlet weak var datePicker: PIDatePicker!
+    @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var label: UILabel!
-    
-    let validPast: NSTimeInterval = -10000000000
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.datePicker.minimumDate = NSDate().dateByAddingTimeInterval(validPast)
-        self.datePicker.delegate = self
+        self.datePicker.minimumDate = NSDate.distantPast()
+        self.datePicker.maximumDate = NSDate.distantFuture()
+    }
+    
+    @IBAction func dateChanged(sender: UIDatePicker) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = .ShortStyle
+        self.label.text = dateFormatter.stringFromDate(datePicker.date)
     }
 
     @IBAction func randomizeColor(sender: AnyObject) {
@@ -30,23 +34,18 @@ class PIViewController : UIViewController {
         let blue = CGFloat(arc4random_uniform(255))
         
         self.datePicker.textColor = UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
-        self.datePicker.reloadAllComponents()
     }
     
     @IBAction func randomizeFont(sender: AnyObject) {
         let familyNames = UIFont.familyNames()
         let randomNumber = Int(arc4random_uniform(UInt32(familyNames.count)))
         let familyName: String = familyNames[randomNumber]
-        let fontName: String = UIFont.fontNamesForFamilyName(familyName)[0]
-        self.datePicker.font = UIFont(name: fontName, size: 14)!
-        self.datePicker.reloadAllComponents()
-    }
-}
-
-extension PIViewController: PIDatePickerDelegate {
-    func pickerView(pickerView: PIDatePicker, didSelectRow row: Int, inComponent component: Int) {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateStyle = .ShortStyle
-        self.label.text = dateFormatter.stringFromDate(pickerView.date)
+        guard let fontName: String = UIFont.fontNamesForFamilyName(familyName).first else {
+            return
+        }
+        guard let font = UIFont(name: fontName, size: 14) else {
+            return
+        }
+        self.datePicker.font = font
     }
 }
